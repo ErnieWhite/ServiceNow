b39c970def0445f97ac64bf4e0369de247ffb958
ErnieWhite <ernie.white.68@gmail.com>
2025-10-26 06:22:38 -0500

    changed argument list to not show the program name

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main(int argc, char **argv) {
     printf("Arguments: %d\n", argc);
     
     int i = 0;
-    for (i = 0; i < argc; i++) {
-        printf("Arg %d - %s\n", i+1, argv[i]);
+    for (i = 1; i < argc; i++) {
+        printf("Arg %d - %s\n", i, argv[i]);
     };
 
     return 0;
