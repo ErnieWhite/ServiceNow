2d57810da87447827d329ed8d4ee19aa08925b68
Ernie White <ernie.white.68@gmail.com>
2025-10-26 06:16:17 -0500

    catching up

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Arguments: %d\n", argc);
+    
+    int i = 0;
+    for (i = 0; i < argc; i++) {
+        printf("Arg %d - %s\n", i+1, argv[i]);
+    };
+
+    return 0;
+}
+
